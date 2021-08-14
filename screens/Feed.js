6469a0038cf7e78fed58360fff6b5045853e6143
kshitij1127 <gudlakshitij.1127@gmail.com>
2021-08-14 19:04:25 +0530

    fourth commit

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,98 +1,99 @@
-import React from "react";
+import * as React from "react";
 import {
   View,
   Text,
   StyleSheet,
-  SafeAreaView,
-  Image,
-  FlatList,
   Platform,
   StatusBar,
+  Image,
+  SafeAreaView,
 } from "react-native";
+
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+import { FlatList } from "react-native-gesture-handler";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import PostCard from "./PostCard";
 
-let posts = require("../temp_posts.json");
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf"),
+};
+
+let posts = require("../temp_posts.json");  
 
 export default class Feed extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+    };
+  }
+
+  async loadFonts() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this.loadFonts();
+  }
+
+  renderItem = ({ item: card }) => {
+    return <PostCard card={card}/>;
+  };
+
   keyExtractor = (item, index) => {
     index.toString();
   };
 
-  renderItem = ({ item: card }) => {
-    return <PostCard card={card} />;
-  };
-
   render() {
-    return (
-      <View style={styles.container}>
-        <SafeAreaView style={styles.droidSafeArea} />
-    
-        <View style={styles.appTitle}>
-        <View style={styles.appTitleTextContainer}>
-          <Text style={styles.appTitleText}>Spectagram</Text>
-        </View>
-
-          <View style={styles.appIcon}>
-            <Image
-              source={require("../assets/logo.png")}
-              style={styles.iconImage}
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              />
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>Spectagram</Text>
+            </View>
+          </View>
+          <View style={styles.cardContainer}>
+            <FlatList
+              keyExtractor={this.keyExtractor}
+              data={posts}
+              renderItem={this.renderItem}
             />
           </View>
+          <View style={{ flex: 0.08 }}></View>
         </View>
-        <View style={styles.cardContainer}>
-          <FlatList
-            keyExtractor={this.keyExtractor}
-            renderItem={this.renderItem}
-            data={posts}
-          />
-        </View>
-      </View>
-    );
+      );
+    }
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
+  container: { flex: 1, backgroundColor: "#15193c" },
   droidSafeArea: {
     marginTop:
       Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
   },
-
-  appTitle: {
-    flex: 1,
-    flexDirection: "row",
-  },
-
-  appIcon: {
-    flex: 0.2,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  appTitleTextContainer: {
-    flex: 1,
-    justifyContent: "center",
-  },
-
-  iconImage: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
-  },
-
+  appTitle: { flex: 0.07, flexDirection: "row" },
+  appIcon: { flex: 0.3, justifyContent: "center", alignItems: "center" },
+  iconImage: { width: "100%", height: "100%", resizeMode: "contain" },
+  appTitleTextContainer: { flex: 0.7, justifyContent: "center" },
   appTitleText: {
+    color: "white",
     fontSize: RFValue(28),
-    coloer: "#ffffff",
+    fontFamily: "Bubblegum-Sans",
   },
-
-  cardContainer: {
-    flex: 0.85,
-  },
+  cardContainer: { flex: 1 },
 });
--- screens/PostCard.js
+++ screens/PostCard.js
@@ -1,80 +1,128 @@
-import React from 'react'
-import { StyleSheet, View, Platform, Image, Text } from 'react-native'
-import { RFValue } from 'react-native-responsive-fontsize'
-import Ionicons from 'react-native-vector-icons/Ionicons'
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 
-export default class PostCard extends React.Component{
-    constructor(props){
-        super(props)
-    }
-    
-    render(){
-        return(
-            <View style={styles.container}>
-                <View style={styles.cardContainer}>
-                    <View style={styles.authorContainer}>
-                        <Image source={require('../assets/profile_img.png')}/>
-                    </View>
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
 
-                    <View>
-                        <Text style={styles.authorNameText}>hello</Text>
-                    </View>
-                </View>
+export default class PostCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false
+    };
+  }
 
-                <Image source={require('../assets/post.jpeg')}/>
-                <View style={styles.captionContainer}>
-                    <Text style={styles.captionText}>hi</Text>
-                </View>
-                <View style={styles.actionContainer}>
-                    <View style={styles.likeButton}>
-                        <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
-                    </View>
-                </View>
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+        <View style={styles.cardContainer}>
+          <Image
+            source={require("../assets/post.jpeg")}
+            style={styles.storyImage}
+          ></Image>
+
+          <View style={styles.titleContainer}>
+            <Text style={styles.storyTitleText}>
+              {this.props.card.title}
+            </Text>
+            <Text style={styles.storyAuthorText}>
+              {this.props.card.author}
+            </Text>
+            <Text style={styles.descriptionText}>
+              {this.props.card.description}
+            </Text>
+          </View>
+          <View style={styles.actionContainer}>
+            <View style={styles.likeButton}>
+              <Ionicons name={"heart"} size={RFValue(30)} color={"white"} />
+              <Text style={styles.likeText}>12k</Text>
             </View>
-        )
+          </View>
+        </View>
+      </View>
+      );
     }
+  }
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-
-    droidSafeArea: {
-        marginTop: Platform.OS === 'android' ? StatusBar.currentHeight : RFValue(35),
-    },
-
-    appTitle: {
-        flex: 0.07,
-        flexDirection: 'row',
-    },
-
-    appIcon: {
-        flex: 0.2,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-    appTitleTextContainer: {
-        flex: 0.8,
-        justifyContent: 'center',
-    },
-
-    iconImage: {
-        width: "100%",
-        height: "100%",
-        resizeMode: "contain",
-    },
-
-    appTitleText: {
-        fontSize: RFValue(28),
-        coloer: "#ffffff",
-    },
-
-    cardContainer: {
-        flex: 0.85,
-    }
-})
+  container: {
+    flex: 1
+  },
+  cardContainer: {
+    margin: RFValue(13),
+    backgroundColor: "#2f345d",
+    borderRadius: RFValue(20)
+  },
+  storyImage: {
+    resizeMode: "contain",
+    width: "95%",
+    alignSelf: "center",
+    height: RFValue(250)
+  },
+  titleContainer: {
+    paddingLeft: RFValue(20),
+    justifyContent: "center"
+  },
+  storyTitleText: {
+    fontSize: RFValue(25),
+    fontFamily: "Bubblegum-Sans",
+    color: "white"
+  },
+  storyAuthorText: {
+    fontSize: RFValue(18),
+    fontFamily: "Bubblegum-Sans",
+    color: "white"
+  },
+  descriptionText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: 13,
+    color: "white",
+    paddingTop: RFValue(10)
+  },
+  actionContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: RFValue(10)
+  },
+  likeButton: {
+    width: RFValue(160),
+    height: RFValue(40),
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#eb3948",
+    borderRadius: RFValue(30)
+  },
+  likeText: {
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    marginLeft: RFValue(5)
+  }
+});
